7224747e6530eb17ee42ce2e3862164f55c45572
edumoritz <eduardo.moritz@hotmail.com>
2021-08-28 15:35:51 -0300

    some improvements

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,3 +1,5 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { HttpClientModule } from "@angular/common/http";
 import { ReactiveFormsModule } from "@angular/forms";
@@ -5,17 +7,29 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LoadingModule } from "@shared/components/loading/loading.module";
 import { DialogService, DynamicDialogModule } from "primeng/dynamicdialog";
+import { AuthModule } from "@features/auth/auth.module";
 
 @NgModule({
 	declarations: [],
 	imports: [
 		CommonModule,
+		BrowserModule,
+		BrowserAnimationsModule,
 		ReactiveFormsModule,
 		HttpClientModule,
 		RouterModule,
 		LoadingModule,
+		AuthModule,
 		DynamicDialogModule
 	],
+	exports: [
+		CommonModule,
+		BrowserModule,
+		BrowserAnimationsModule,
+		ReactiveFormsModule,
+		HttpClientModule
+	],
+
 	providers: [DialogService]
 })
 export class CoreModule {}
